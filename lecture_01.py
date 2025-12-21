4402cb9365bfcd71a46ad16d2df43e17d5a54cbe
Fuaad <fuaadmohamoud@gmail.com>
2025-12-21 15:30:05 -0600

    Excercise 3

--- lecture_01.py
+++ lecture_01.py
@@ -3,8 +3,20 @@ def exercise_1():
     type(5)
     print(3.0-1)
 
+def excercise_3():
+    usa_gold = 46
+    uk_gold = 27
+    romania_gold = 1
+
+    total_gold = usa_gold + uk_gold + romania_gold
+    print(total_gold)
+
+    romania_gold += 1
+    print(total_gold)
+
 def main():
-    excercise_1()
+    excercise_3()
+
 
 
 
