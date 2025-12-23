64e955220484c07c9a17af593a53830c261ade38
Fuaad <fuaadmohamoud@gmail.com>
2025-12-23 14:02:52 -0600

    changed some input variables to be constants

--- ps1c.py
+++ ps1c.py
@@ -11,8 +11,8 @@ def main():
   
     annual_salary = get_float("Enter your starting annual salary: ","Annual salary needs to be a number")
     portion_saved = get_float("Enter the percent of your salary to save, as a decimal: ","Portion Saved needs to be a number")
-    total_cost = get_float("Enter the cost of your dream home: ","Total cost needs to be a number")
-    semi_annual_raise = get_float("Enter the semi-annual raise, as a decimal: ", "Your semi-annual raise needs to be a number")
+    total_cost = 1000000
+    semi_annual_raise = .07
     portion_down_payment = .25 * total_cost
     current_savings = 0.0
     
