6b372c27d608ea62e0e94a4508c24df061074fd0
Fuaad <fuaadmohamoud@gmail.com>
2025-12-22 18:56:38 -0600

    refactored

--- ps1a.py
+++ ps1a.py
@@ -1,22 +1,27 @@
 
 
 
-if __name__ == "__main__":
-    main()
 
-    
 def main():
-    total_cost = float(input("what is the cost of your dream house?"))
-    portion_down_payment = 25000.0
+    annual_salary = float(input("Enter your annual salary: "))
+    portion_saved = float(input("Enter the percent of your salary to save, as a decimal: "))
+    total_cost = float(input("Enter the cost of your dream home: "))
+    portion_down_payment = .25 * total_cost
     current_savings = 0.0
-    annual_salary = float(input("What is your annual salary"))
-    portion_saved = float(input("What percentage of it do you save every month"))
-    return_on_investment =  current_savings * (.04 / 12)
+    
+    
     months = 0
 
-    while current_savings < total_cost:
+    while current_savings < portion_down_payment:
+        return_on_investment =  current_savings * (.04 / 12)
         months+=1
-        current_savings+= (annual_salary * portion_saved) + (current_savings * return_on_investment)
+        monthly_saved = (annual_salary / 12) * portion_saved
+        current_savings+= monthly_saved + return_on_investment
         
     print (f"Number of months: {months}")
 
+
+
+
+if __name__ == "__main__":
+    main()
