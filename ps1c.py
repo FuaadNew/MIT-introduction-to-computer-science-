ee697f9c83ad3b4c542e61c9f367060d283c888a
Fuaad <fuaadmohamoud@gmail.com>
2025-12-23 14:49:46 -0600

    more refactoring

--- ps1c.py
+++ ps1c.py
@@ -3,12 +3,6 @@
 
 
 def main():
-    annual_salary = get_float("Enter your starting annual salary: ","Annual salary needs to be a number")
-    portion_saved = get_float("Enter the percent of your salary to save, as a decimal: ","Portion Saved needs to be a number")
-    total_cost = 1000000
-    semi_annual_raise = .07
-    portion_down_payment = .25 * total_cost
-    current_savings = 0
 
     def get_float(prompt, error_message):
         while True:
@@ -17,10 +11,21 @@ def main():
             except ValueError:
                 print(error_message)
 
+    annual_salary = get_float("Enter your starting annual salary: ","Annual salary needs to be a number")
+    portion_saved = get_float("Enter the percent of your salary to save, as a decimal: ","Portion Saved needs to be a number")
+    total_cost = 1000000
+    semi_annual_raise = .07
+    down_payment = .25 * total_cost
+    current_savings = 0
+
     
-    months = 0
+    
+    
     def test_saving_rate(saving_rate):
-        while current_savings < portion_down_payment:
+        months = 0
+        while current_savings < down_payment:
+            if down_payment - 100 <= current_savings <= down_payment + 100:
+                break
             months+=1
             return_on_investment = current_savings * (saving_rate / 12)
             monthly_saved = (annual_salary / 12) * portion_saved
@@ -28,9 +33,37 @@ def main():
             if not months % 6:
                 annual_salary+= (semi_annual_raise * annual_salary)
 
-        return months
+        return months <= 36
+    
+    def binary_search():
+        l,r = 0,1
+
+        while l<r:
+            mid = (l + r) / 2
+            months = test_saving_rate(saving_rate)
+
+            if months < 36:
+                r = mid
+            else:
+                l = mid + .01
+        
+        if months > 36:
+            print("it's not possible")
+        else:
+            print(l)
+    binary_search()
+        
+            
+
+    
+
+    
+
+
 
         
+    
+
 
   
    
