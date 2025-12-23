25a43b4d404b15be99cd8eb08a36331122be22f0
Fuaad <fuaadmohamoud@gmail.com>
2025-12-23 13:41:47 -0600

    copied over ps1a to ps1b

---
+++ ps1b.py
@@ -0,0 +1,33 @@
+
+def get_float(prompt, error_message):
+    while True:
+        try:
+            return float(input(prompt))
+        except ValueError:
+            print(error_message)
+
+
+def main():
+  
+    annual_salary = get_float("Enter your annual salary: ","Annual salary needs to be a number")
+    portion_saved = get_float("Enter the percent of your salary to save, as a decimal: ","Portion Saved needs to be a number")
+    total_cost = get_float("Enter the cost of your dream home: ","Total cost needs to be a number")
+    portion_down_payment = .25 * total_cost
+    current_savings = 0.0
+    
+    
+    months = 0
+
+    while current_savings < portion_down_payment:
+        return_on_investment = current_savings * (.04 / 12)
+        months+=1
+        monthly_saved = (annual_salary / 12) * portion_saved
+        current_savings+= monthly_saved + return_on_investment
+        
+    print (f"Number of months: {months}")
+
+
+
+
+if __name__ == "__main__":
+    main()
