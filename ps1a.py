b8eddf9592a165d1e5ba3fec91dc91fd7483fc3d
Fuaad <fuaadmohamoud@gmail.com>
2025-12-22 18:36:32 -0600

    missing variable

--- ps1a.py
+++ ps1a.py
@@ -1,17 +1,19 @@
 
 def main():
-    total_cost = input("what is the cost of your dream house?")
+    total_cost = float(input("what is the cost of your dream house?"))
     portion_down_payment = 25000.0
     current_savings = 0.0
-    annual_salary = input("What is your annual salary")
-    portion_saved = input("What percentage of it do you save every month")
+    annual_salary = float(input("What is your annual salary"))
+    portion_saved = float(input("What percentage of it do you save every month"))
     return_on_investment =  current_savings * (.04 / 12)
+    number_of_months = 0
 
     
-    for i in range(12):
+    
 
 
 
+    return "Number of months: " + number_of_months
 
 if __name__ == "__main__":
     main()
