801687761f2770dfc79288e1e6f50b6d8683bc65
Fuaad <fuaadmohamoud@gmail.com>
2025-12-23 14:21:30 -0600

    first draft of binary search function

--- ps1c.py
+++ ps1c.py
@@ -7,6 +7,29 @@ def get_float(prompt, error_message):
             print(error_message)
 
 
+def testSavings(mid):
+
+def binary_search(epsilon,goal,annual_salary):
+    
+    l,r = 0.0,1.0
+    
+
+    while l<r:
+        mid = (l + r) / 2
+        test_months = testSavings(mid)
+        if goal - epsilon <= test_months <= goal + epsilon:
+            print("steps")
+        else:
+            if test_months > goal:
+                l = mid + .01
+            else:
+                r = mid - 1
+    print("there is no way to hit the goal")
+
+
+
+
+
 def main():
   
     annual_salary = get_float("Enter your starting annual salary: ","Annual salary needs to be a number")
@@ -14,5 +37,5 @@ def main():
     total_cost = 1000000
     semi_annual_raise = .07
     portion_down_payment = .25 * total_cost
-    current_savings = 0.0
     
+
