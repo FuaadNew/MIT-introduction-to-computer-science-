f925d40469be99cc554a56388ed9cf8ee281b2e8
Fuaad <fuaadmohamoud@gmail.com>
2025-12-22 18:32:06 -0600

    first half of ps1a

---
+++ ps1a.py
@@ -0,0 +1,13 @@
+
+def main():
+    total_cost = 100000
+    portion_down_payment = 25000
+    current_savings = 0
+    annual_salary = 50000
+    portion_saved = .10
+    savings_rate =  current_savings * (.04 / 12)
+
+    
+
+if __name__ == "__main__":
+    main()
