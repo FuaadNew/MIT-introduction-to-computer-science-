2977d221c1bd9f94a20ec5acbcea3b0ac53e6c9b
Fuaad <fuaadmohamoud@gmail.com>
2025-12-24 09:33:52 -0600

    recompleted ps1c.py

--- ps1c.py
+++ ps1c.py
@@ -14,19 +14,21 @@ def main():
     annual_return = 0.04
     down_payment = 0.25 
     cost = 1000000 
+    goal = cost * down_payment
 
     annual_salary = get_float("Enter the starting salary: ","Annual salary needs to be a number")
 
 
 
-    def check_savings_rate(saving_rate):
+    def check_savings_rate(saving_rate, annual_salary):
     
         months = 0
+        current_savings = 0 
 
-        while current_savings < portion_down_payment:
+        while months < 36:
             months+=1
-            return_on_investment = current_savings * (saving_rate / 12)
-            monthly_saved = (annual_salary / 12) * portion_saved
+            return_on_investment = current_savings * (.04 / 12)
+            monthly_saved = (annual_salary / 12) * saving_rate
             current_savings+= monthly_saved + return_on_investment
             if not months % 6:
                 annual_salary+= (semi_annual_raise * annual_salary)
@@ -35,9 +37,44 @@ def main():
 
 
 
+    def binary_search(annual_salary):
+
+        test_savings = check_savings_rate(1, annual_salary)
+        if test_savings < goal - 100:
+            print("It is not possible to pay the down payment in three years.")
+            return
+
+
+
+        l,r = 0, 10000
+        steps = 0
+
+        while l < r:
+            mid = (l + r) // 2
+
+            savings = check_savings_rate(mid / 10000 , annual_salary)
+            print(mid / 10000)
+
+            if savings < goal - 100:
+                l = mid + 1
+            else:
+                r  = mid - 1
+            steps+=1
+        print("Best savings rate:", mid / 10000)
+        print("Steps in bisection search:", steps)
+        
 
 
 
 
-if __name__ == "__main__"":
+
+
+    binary_search(annual_salary)
+
+
+
+
+
+
+if __name__ == "__main__":
     main()
