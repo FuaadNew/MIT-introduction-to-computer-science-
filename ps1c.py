1812a8a0792c2a498bd92d58ee6bbac086a23ec7
Fuaad <fuaadmohamoud@gmail.com>
2025-12-24 08:05:03 -0600

    made check_savings_rate function

--- ps1c.py
+++ ps1c.py
@@ -16,7 +16,22 @@ def main():
     cost = 1000000 
 
     annual_salary = get_float("Enter the starting salary: ","Annual salary needs to be a number")
+
+
+
+    def check_savings_rate(saving_rate):
     
+        months = 0
+
+        while current_savings < portion_down_payment:
+            months+=1
+            return_on_investment = current_savings * (saving_rate / 12)
+            monthly_saved = (annual_salary / 12) * portion_saved
+            current_savings+= monthly_saved + return_on_investment
+            if not months % 6:
+                annual_salary+= (semi_annual_raise * annual_salary)
+        
+        return current_savings
 
 
 
